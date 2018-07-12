02939b030ba1e21ce18af780e110c1b26179e731
Charly Mannion <charlottemannion100@gmail.com>
2018-07-12 18:44:18 +0100

    Playing the lyrics returns the first line of the song ten green bottles

---
+++ src/lyrics.js
@@ -0,0 +1,7 @@
+function Lyrics() {
+
+}
+
+Lyrics.prototype.play = function() {
+  return "10 Green Bottles Hanging on the Wall. 10 Green Bottles Hanging on the Wall. And if one green bottle should accidentally fall, there'll be 9 Green Bottles Hanging on the Wall." 
+}
