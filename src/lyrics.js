af0d8ce9079479dfd0162a2da6a6acd042e3efe1
Charly Mannion <charlottemannion100@gmail.com>
2018-07-12 18:46:17 +0100

    Adds console.log() to print the lyrics to the javascript console

--- src/lyrics.js
+++ src/lyrics.js
@@ -3,5 +3,7 @@ function Lyrics() {
 }
 
 Lyrics.prototype.play = function() {
-  return "10 Green Bottles Hanging on the Wall. 10 Green Bottles Hanging on the Wall. And if one green bottle should accidentally fall, there'll be 9 Green Bottles Hanging on the Wall." 
+  return "10 Green Bottles Hanging on the Wall. 10 Green Bottles Hanging on the Wall. And if one green bottle should accidentally fall, there'll be 9 Green Bottles Hanging on the Wall."
 }
+
+console.log(Lyrics.prototype.play())
